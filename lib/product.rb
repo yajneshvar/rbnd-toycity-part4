3882b404bd454113eb6ca044a798a6db168875ab
yaj <yajneshvar.arumugam@gmail.com>
2016-06-26 09:46:05 -0400

    First and all method working for Product class

--- lib/product.rb
+++ lib/product.rb
@@ -25,10 +25,19 @@ class Product < Udacidata
     return product
   end
 
-  def self.first
+  def self.first(count=1)
     faster_csv = CSV.open(Udacidata.data_path,{headers: true, return_headers: false})
-    info = faster_csv.first
-    product = Product.new({id: info[0],brand: info[1], product: info[2], price: info[3] })
+    info = faster_csv.first(count)
+    puts info
+    if(count == 1)
+      return Product.new({id: info[0],brand: info[1], product: info[2], price: info[3] })
+    else
+      product = []
+      info.each do |attr|
+        product << Product.new({id: attr[0],brand: attr[1], product: attr[2], price: attr[3] })
+      end
+      return product
+    end
   end
 
   private
